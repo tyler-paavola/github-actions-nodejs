57fe7b3f0f7b7cfd50664b2b027ade238fec5d14
Tyler Paavola <Tyler.Paavola@umanitoba.ca>
2019-12-09 21:56:27 -0600

    Fix copy/paste woes

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   var nameToGreet = core.getInput('who-to-greet');
-  console.log('Hello ${nameToGreet}!');
+  console.log(`Hello ${nameToGreet}!`);
   var time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   var payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log('The event payload: ${payload}');
+  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
